c088b02331bdb674a0a7d46d8a2091881dec95cf
floh-bhaptics <floh@fahrenbergers.de>
2022-01-10 21:08:30 +0100

    Projektdateien hinzufügen.

---
+++ GunClubVR_bhaptics.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MelonLoader;
+using HarmonyLib;
+using MyBhapticsTactsuit;
+
+namespace GunClubVR_bhaptics
+{
+    public class GunClubVR_bhaptics : MelonMod
+    {
+        public static TactsuitVR tactsuitVr;
+
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+            tactsuitVr = new TactsuitVR();
+            tactsuitVr.PlaybackHaptics("HeartBeat");
+        }
+
+        [HarmonyPatch(typeof(WeaponInteraction), "FireHaptics", new Type[] { typeof(int) })]
+        public class bhaptics_WeaponHandlerFire
+        {
+            [HarmonyPostfix]
+            public static void Postfix(WeaponInteraction __instance, int side)
+            {
+                tactsuitVr.LOG("Fire side: " + side.ToString());
+                tactsuitVr.Recoil("Pistol", true);
+            }
+        }
+
+    }
+}
