a8fe010918be6e6705878e5d18d475b467c0a2c0
floh-bhaptics <floh@fahrenbergers.de>
2022-01-11 13:28:36 +0100

    Added very hacky damage and explosions

--- GunClubVR_bhaptics.cs
+++ GunClubVR_bhaptics.cs
@@ -22,19 +22,54 @@ namespace GunClubVR_bhaptics
         }
 
         [HarmonyPatch(typeof(TBMVRDevice), "FireHaptics", new Type[] { typeof(Side), typeof(VibrationForce) })]
-        public class bhaptics_WeaponHandlerFire3
+        public class bhaptics_FireHaptics
         {
             [HarmonyPostfix]
-            public static void Postfix(Side handSide, VibrationForce force)
+            public static void Postfix(TBMVRDevice __instance, Side handSide, VibrationForce force)
             {
                 if ((force == VibrationForce.Light) | (force == VibrationForce.None)) return;
                 float intensity = 1.0f;
-                if (force == VibrationForce.Medium) intensity = 0.7f;
+                if (force == VibrationForce.Medium) { intensity = 0.7f; }
                 tactsuitVr.Recoil("Pistol", (handSide == Side.Right), intensity);
-                //tactsuitVr.LOG("Haptics: " + handSide.ToString() + " " + force.ToString());
+            }
+        }
+        
+        [HarmonyPatch(typeof(DestructableHandler), "DamageSurroundings", new Type[] { typeof(SpawnedRangeHandler), typeof(UnityEngine.Transform), typeof(int), typeof(int) })]
+        public class bhaptics_ExplosionDamage
+        {
+            [HarmonyPostfix]
+            public static void Postfix(DestructableHandler __instance)
+            {
+                tactsuitVr.PlaybackHaptics("ExplosionBelly");
             }
         }
 
-
+        /*
+        [HarmonyPatch(typeof(AudioHandler), "PlaySound", new Type[] { typeof(UnityEngine.AudioClip) })]
+        public class bhaptics_PlaySound5
+        {
+            [HarmonyPostfix]
+            public static void Postfix(UnityEngine.AudioClip soundClip)
+            {
+                // aUILevelUpExplosion
+                // 
+                if (soundClip.name == "aUIPlayerDamaged") tactsuitVr.PlaybackHaptics("Impact");
+                tactsuitVr.LOG("Audio5 " + soundClip.name);
+            }
+        }
+        */
+        [HarmonyPatch(typeof(AudioHandler), "PlaySoundAtPoint", new Type[] { typeof(UnityEngine.Vector3), typeof(UnityEngine.AudioClip) })]
+        public class bhaptics_PlaySound6
+        {
+            [HarmonyPostfix]
+            public static void Postfix(UnityEngine.AudioClip soundClip)
+            {
+                // aUILevelUpExplosion
+                // 
+                if (soundClip.name.Contains("Hurt")) tactsuitVr.PlaybackHaptics("Impact");
+                //tactsuitVr.LOG("Audio6 " + soundClip.name);
+            }
+        }
+        
     }
 }
